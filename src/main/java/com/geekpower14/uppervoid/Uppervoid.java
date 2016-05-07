1e73c279ebe02b017cd78c30338e5c3de8c1a318
frasse_l <frasse_l@epitech.eu>
2016-05-07 12:00:02 +0200

    Fixed stat names

--- src/main/java/com/geekpower14/uppervoid/Uppervoid.java
+++ src/main/java/com/geekpower14/uppervoid/Uppervoid.java
@@ -3,6 +3,7 @@ package com.geekpower14.uppervoid;
 import com.geekpower14.uppervoid.arena.Arena;
 import com.geekpower14.uppervoid.listener.PlayerListener;
 import net.samagames.api.SamaGamesAPI;
+import net.samagames.api.games.GamesNames;
 import net.samagames.api.stats.IStatsManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -18,7 +19,7 @@ public class Uppervoid extends JavaPlugin
 
         Bukkit.getPluginManager().registerEvents(new PlayerListener(this, this.arena), this);
         SamaGamesAPI.get().getGameManager().registerGame(this.arena);
-        SamaGamesAPI.get().getStatsManager().setStatsToLoad(IStatsManager.StatsNames.UPPERVOID, true);
+        SamaGamesAPI.get().getStatsManager().setStatsToLoad(GamesNames.UPPERVOID, true);
     }
 
     public Arena getArena()

2c853c9a19730623cae701226ae25c37ee2b9ffa
frasse_l <frasse_l@epitech.eu>
2016-05-07 12:00:54 +0200

    Removed useless import

--- src/main/java/com/geekpower14/uppervoid/Uppervoid.java
+++ src/main/java/com/geekpower14/uppervoid/Uppervoid.java
@@ -4,7 +4,6 @@ import com.geekpower14.uppervoid.arena.Arena;
 import com.geekpower14.uppervoid.listener.PlayerListener;
 import net.samagames.api.SamaGamesAPI;
 import net.samagames.api.games.GamesNames;
-import net.samagames.api.stats.IStatsManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 

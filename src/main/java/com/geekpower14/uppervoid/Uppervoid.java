00c5e9976a0d81d39755d130afee1de0f1dca9f7
Silvanosky <geekpower14@gmail.com>
2016-05-11 01:56:03 +0200

    Add shop load

--- src/main/java/com/geekpower14/uppervoid/Uppervoid.java
+++ src/main/java/com/geekpower14/uppervoid/Uppervoid.java
@@ -19,6 +19,7 @@ public class Uppervoid extends JavaPlugin
         Bukkit.getPluginManager().registerEvents(new PlayerListener(this, this.arena), this);
         SamaGamesAPI.get().getGameManager().registerGame(this.arena);
         SamaGamesAPI.get().getStatsManager().setStatsToLoad(GamesNames.UPPERVOID, true);
+        SamaGamesAPI.get().getShopsManager().setShopToLoad(GamesNames.UPPERVOID, true);
     }
 
     public Arena getArena()
